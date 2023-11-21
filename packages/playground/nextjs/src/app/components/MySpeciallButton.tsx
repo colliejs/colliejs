3af4852ffda1a6b14bc2bfcbf93af434e0eb5eb2
border-collie <colliedog1001@gmail.com>
2023-11-21 09:05:23 +0800

    refactor: exmaple ref

--- packages/playground/nextjs/src/app/components/MySpeciallButton.tsx
+++ packages/playground/nextjs/src/app/components/MySpeciallButton.tsx
@@ -1,7 +1,10 @@
+//TODO:找不到类型
+
 //@ts-ignore
 import { styled } from "@colliejs/react";
 import { MyButton } from "./MyButton";
 export const MySpecialButton = styled(MyButton, {
   background: "blue",
   fontSize: 30,
+  borderRadius: 10,
 });
--- packages/playground/nextjs/src/app/page.tsx
+++ packages/playground/nextjs/src/app/page.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
-import styles from "./page.module.css";
 import { Button } from "./components/Button";
 import { MyButton } from "./components/MyButton";
 import { MySpecialButton } from "./components/MySpeciallButton";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
       <Button>Button</Button>
       <MyButton>MyBUtton</MyButton>
       <MySpecialButton>MySpecialButton</MySpecialButton>
