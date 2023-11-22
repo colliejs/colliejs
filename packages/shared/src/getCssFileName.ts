526504bde1cbe15beca1f89d52799bb49c1fa81f
border-collie <colliedog1001@gmail.com>
2023-11-22 09:37:50 +0800

    chore: release

--- packages/shared/src/getCssFileName.ts
+++ packages/shared/src/getCssFileName.ts
@@ -1,9 +1,15 @@
 import path from "node:path";
 import { toHash } from "./toHash";
-export const getCssFileName = (url: string) => {
+export const getCssFileName = (url: string, enableTimeStampe = true) => {
   const lastSeg = path.dirname(url).split("/").pop();
   return (baseDir: string) => {
     const prefix = path.resolve(baseDir, "collie-cache");
-    return `${prefix}/${lastSeg}-${path.basename(url)}-${toHash(url)}.css`;
+    const newUrl = `${prefix}/${lastSeg}-${path.basename(url)}-${toHash(
+      url
+    )}.css`;
+    if (!enableTimeStampe) {
+      return newUrl;
+    }
+    return `${newUrl}?stamp=${Date.now()}`
   };
 };
