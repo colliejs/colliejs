ac1a66f9b7b5cce82534088830fcb4b98967b268
colliejs <136399756+colliejs@users.noreply.github.com>
2024-01-07 18:12:46 +0800

    fix: refactor (#13)

---
+++ examples/vite/src/components/Img.tsx
@@ -0,0 +1,29 @@
+import { styled } from "../styled";
+export const ImageButton = styled(
+  "img",
+  {
+    w: 100,
+    h: 100,
+    variants: {
+      type: {
+        primary: {
+          background: "red",
+        },
+        secondary: {
+          background: "blue",
+        },
+      },
+    },
+  },
+  {
+    as: "button",
+    disabled: true,
+  }
+);
+
+type A = React.ComponentProps<typeof ImageButton>;
+type x = React.DialogHTMLAttributes<HTMLDialogElement>;
+type a = A["form"];
+const X: React.FC = () => {
+  return <ImageButton open />;
+};
