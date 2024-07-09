5fbf4edc7e88f87c347c259bd43d24133c27d3e3
colliejs <colliedog001@gmail.com>
2024-07-09 14:33:37 +0800

    chore: update tsconfig.node.json and add missing file to include

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -129,7 +129,7 @@ run({
     writeFile(".gitignore", `${cssRoot}/.collie/\n`);
     await addWatchToPackageJson();
     if (usingTs) {
-      await addConfigFileToTsConfigIfNeeded("tsconfig.json");
+      await addConfigFileToTsConfigIfNeeded("tsconfig.node.json");
     }
     consola.success("collie init done");
     consola.info(`==>next: run 'npm run dev' to start dev server`);
